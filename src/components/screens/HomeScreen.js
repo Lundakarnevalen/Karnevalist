837053ec3161ce4b9c14b94c4b2b6f1e2156db2a
E-mil <limeEB96>
2017-11-29 19:07:41 +0100

    Changed the landing page to the latest design

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -8,7 +8,9 @@ class HomeScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      alertVisible: false
+      alertVisible: false,
+      socSecNbr: '',
+      password: ''
     }
   }
 
@@ -16,28 +18,55 @@ class HomeScreen extends Component {
     return (
       <Image 
         style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-        source={require('../../../res/HomeScreenBackground.jpg')}
+        source={require('../../../res/Flicka_Tuba_Byggnader.png')}
       >
-        <View style={styles.container2}>
-          <CustomButton
-            text='Change to English'
-            textStyle={{
-              color: 'blue'
-            }}
-            buttonStyle={styles.transparentButtonStyle}
-          />
-        </View>
+        <View 
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            backgroundColor: '#8A4797',
+            height: Dimensions.get('window').height,
+            width: Dimensions.get('window').width,
+            opacity: 0.7
+          }} 
+        />
         <View style={styles.container1}>
+          <Image
+            source={require('../../../res/LK2018logga.png')}
+          />
           <Input
+            placeholder='Personnummer'
             title='Personnummer'
             width={Dimensions.get('window').width * 0.9}
-            viewStyle={{ marginBottom: 10 }}
+            viewStyle={{ marginBottom: 2 }}
+            textInputStyle={styles.textInputStyle}
+            headerTextStyle={styles.inputHeaderTextStyle}
+            underlineColorAndroid='transparent'
+            onChangeText={
+              text => {
+                return this.setState(() => {
+                  return { socSecNbr: { text } }
+                });
+              }
+            }
           />
           <Input
+            placeholder='Lösenord'
             title='Lösenord'
             width={Dimensions.get('window').width * 0.9}
             secureText={true}
-            viewStyle={{ marginBottom: 10 }}
+            viewStyle={{ marginBottom: 11 }}
+            textInputStyle={styles.textInputStyle}
+            headerTextStyle={styles.inputHeaderTextStyle}
+            underlineColorAndroid='transparent'
+            onChangeText={
+              text => {
+                return this.setState(() => {
+                  return { password: { text } }
+                });
+              }
+            }
           />
           <CustomButton
             text='Logga in'
@@ -48,9 +77,12 @@ class HomeScreen extends Component {
               color: 'white',
               fontSize: 20
             }}
-            buttonStyle={[styles.normalButtonStyle, { marginBottom: 30 }]}
+            buttonStyle={[styles.normalButtonStyle, 
+              { marginBottom: Dimensions.get('window').height * (1 / 14) }]}
           />
-          <Text>
+          <Text
+            style={{ color: 'white', fontSize: 12 }}
+          >
             Har du ingen profil?
           </Text>
           <CustomButton
@@ -59,14 +91,15 @@ class HomeScreen extends Component {
               color: 'white',
               fontSize: 20
             }}
-            buttonStyle={styles.normalButtonStyle}
+            buttonStyle={[styles.normalButtonStyle, { marginBottom: 10 }]}
           />
           <CustomButton
             text='Läs mer om registreringen'
             textStyle={{
-              color: 'blue'
+              color: 'white',
+              fontSize: 12
             }}
-            buttonStyle={styles.transparentButtonStyle}
+            buttonStyle={[styles.transparentButtonStyle, { marginTop: 10 }]}
           />
         </View>
       </Image>
@@ -84,17 +117,28 @@ const styles = {
     flex: 0,
     margin: 0,
     justifyContent: 'flex-start',
-    alignItems: 'flex-start'
+    alignItems: 'flex-end'
   },
   normalButtonStyle: {
     backgroundColor: '#f4376d',
     padding: 10,
-    width: Dimensions.get('window').width * 0.9
+    width: Dimensions.get('window').width * 0.9,
+    borderRadius: 0,
+    borderWidth: 1,
+    borderColor: 'white'
   },
   transparentButtonStyle: {
     marginTop: 30,
     backgroundColor: 'transparent',
     borderWidth: 0
+  },
+  textInputStyle: {
+    borderRadius: 0,
+    borderWidth: 0
+  },
+  inputHeaderTextStyle: {
+    color: 'white',
+    fontSize: 12
   }
 };
 
