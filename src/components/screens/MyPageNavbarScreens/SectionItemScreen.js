90be84538c9992e20b562ab1e6f3baa74345a4da
easklund <dat14eas@student.lu.se>
2017-11-30 10:20:31 +0100

    Chenged color and size on title, and changed to a symbol on add to list button

--- src/components/screens/MyPageNavbarScreens/SectionItemScreen.js
+++ src/components/screens/MyPageNavbarScreens/SectionItemScreen.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { View, Alert, TouchableOpacity, Text, Dimensions, ScrollView, Platform } from 'react-native';
+import { View, Alert, TouchableOpacity, Text, Dimensions, ScrollView, Platform, Image } from 'react-native';
+import { MaterialIcons} from '@expo/vector-icons'
 import { Constants } from 'expo';
 import Header from '../../common/Header'
 
 const WIDTH = Dimensions.get('window').width
 const HEIGHT = Dimensions.get('window').height
+
+const { width } = Dimensions.get('window')
+const { height } = Dimensions.get('window')
 
 class SectionItemScreen extends Component {
   render() {
@@ -17,19 +21,22 @@ class SectionItemScreen extends Component {
         <Header
         title={title}
         style={{ backgroundColor: '#8A4797' }}
-        textStyle={{ fontSize: 30 }}
+        textStyle={{ color: '#FBBCC0' }}
         navigation={navigation}
         rightIcon={
           <TouchableOpacity
-           style={{ padding: 10, backgroundColor: 'red' }}
+           style={{ padding: 1, backgroundColor: '#8A4797' }}
            onPress={() => Alert.alert('Tillagd')}
           >
-            <Text>Lägg till</Text>
+             < MaterialIcons name='playlist-add' size={30} />
           </TouchableOpacity>
         }
         />
         <View style={[styles.container, { }]}>
-          {image}
+        <Image
+          style={styles.imageStyle}
+          source={require('../../../../res/Monstergubbe.png')}
+        />
           <ScrollView style={styles.someStyle}>
             <Text style={{ fontSize: 20, margin: 4 }}>{description}</Text>
           </ScrollView>
@@ -45,6 +52,11 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center',
     width: Dimensions.get('window').width
+  },
+  imageStyle: {
+    resizeMode: 'contain',
+    width: width / 1.1,
+    height: height / 2.3
   },
   someStyle: {
     maxHeight: Platform.os === 'ios' ? HEIGHT - WIDTH - 67.5 - 15 : HEIGHT - WIDTH - 5 - Constants.statusBarHeight,
