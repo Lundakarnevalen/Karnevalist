ed56623894bb8c64f0451d6f096c9cc69d1e9eeb
Love Malmros <love.malmros@gmail.com>
2017-12-17 17:22:57 +0100

    add coming soon text on songbook page

--- src/components/screens/MyPageNavbarScreens/SongBookScreen.js
+++ src/components/screens/MyPageNavbarScreens/SongBookScreen.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, Platform, Dimensions } from 'react-native';
 import Header from '../../common/Header';
 import BackgroundImage from '../../common/BackgroundImage';
 
+const HEIGHT = Dimensions.get('window').height;
+
 class SongBookScreen extends Component {
+  getColor() {
+    return this.props.theme === 'day' ? '#f4376d' : '#F7A021';
+  }
+
   render() {
     return (
       <View>
         <BackgroundImage pictureNumber={2} />
         <Header title="Sångbok" leftIcon={null} navigation={this.props.navigation} />
+        <Text style={[styles.textStyle, { color: this.getColor() }]}>Coming soon!</Text>
       </View>
     );
   }
 }
+
+const styles = {
+  textStyle: {
+    textAlign: 'center',
+    backgroundColor: 'transparent',
+    fontFamily: 'Avenir Next Bold',
+    fontSize: 36,
+    marginTop: HEIGHT / 3
+  }
+};
 
 export default SongBookScreen;
