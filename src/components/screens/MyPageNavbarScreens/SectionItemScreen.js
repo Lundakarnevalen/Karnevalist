b3f879e35fab89c1c01a7116e69e4dd3cf2a6822
Kevinatorn <kevin.kimaryo@gmail.com>
2018-01-20 15:25:43 +0100

    wip

--- src/components/screens/MyPageNavbarScreens/SectionItemScreen.js
+++ src/components/screens/MyPageNavbarScreens/SectionItemScreen.js
@@ -17,7 +17,7 @@ import { saveItem, removeItem, getItem } from '../../../helpers/LocalSave';
 import { SECTION_ITEM_SCREEN_STRINGS } from '../../../helpers/LanguageStrings';
 
 const HEIGHT = Dimensions.get('window').height;
-
+const WIDTH = Dimensions.get('window').width;
 class SectionItemScreen extends Component {
   componentWillMount() {
     const { id } = this.props.navigation.state.params;
@@ -84,37 +84,38 @@ class SectionItemScreen extends Component {
   render() {
     const { navigation } = this.props;
     const { title, description, image, id } = navigation.state.params;
-    const { container, scrollStyle, headerStyle, textStyle } = styles;
+    const { containerStyle, imageStyle, headerStyle, textStyle } = styles;
     return (
-      <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={containerStyle}>
         <Header title={title} navigation={navigation} rightIcon={this.renderRightIcon(id, title)} />
-        <View>
-          <ScrollView style={scrollStyle}>
-            <View style={container}>{image}</View>
-            <View style={{ height: 10, backgroundColor: 'white' }} />
-            <Text style={[headerStyle, { color: '#F7A021' }]}>{title}</Text>
-            <Text style={textStyle}>{description}</Text>
-          </ScrollView>
-          <Toast
-            showToast={this.state.showToast}
-            onClose={() => this.setState({ showToast: false })}
-            message={this.renderToastMessage(title)}
-          />
-        </View>
+        <ScrollView>
+          <View style={imageStyle}>{image}</View>
+          <Text style={[headerStyle, { color: '#F7A021' }]}>{title}</Text>
+          <Text style={textStyle}>{description}</Text>
+        </ScrollView>
+        <Toast
+          showToast={this.state.showToast}
+          onClose={() => this.setState({ showToast: false })}
+          message={this.renderToastMessage(title)}
+        />
       </View>
     );
   }
 }
 
 const styles = {
-  container: {
+  containerStyle: {
+    height: HEIGHT,
+    width: WIDTH,
+    alignItems: 'center'
+  },
+  imageStyle: {
     marginTop: 5,
     alignItems: 'center',
     justifyContent: 'center',
-    width: Dimensions.get('window').width
-  },
-  scrollStyle: {
-    maxHeight: Platform.OS === 'ios' ? HEIGHT : HEIGHT - Constants.statusBarHeight
+    width: WIDTH - 5,
+    height: WIDTH - 5,
+    resizeMode: 'contain'
   },
   headerStyle: {
     fontSize: 26,
