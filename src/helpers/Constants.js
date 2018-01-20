a2b21f6183baaea0eb02d610cc19d27ae009ba52
antongoransson <anton.goransson95@gmail.com>
2018-01-20 14:06:08 +0100

    Added constants file

---
+++ src/helpers/Constants.js
@@ -0,0 +1,13 @@
+export const TOKEN_URL = 'https://api.10av10.com/api/hello/';
+export const SECTION_URL = 'http://lundakarnevalen.se/wp-json/wp/v2/lksektion/';
+export const IMAGE_URL = 'http://lundakarnevalen.se/wp-json/wp/v2/media/';
+export const REGISTER_URL = 'https://api.10av10.com/register';
+export const SECTION_PRIORITY_URL = 'https://api.10av10.com/api/section/';
+export const FORGOT_PASSWORD_URL = 'https://api.10av10.com/login/forgotpassword';
+export const LOGIN_URL = 'https://api.10av10.com/login/email';
+export const USER_URL = 'https://api.10av10.com/api/user/';
+export const PROGRESS = Object.freeze({
+  CHECK_IN: 1,
+  CHOOSE_SECCTIONS: 2,
+  SENT_SECTIONS: 3
+})
