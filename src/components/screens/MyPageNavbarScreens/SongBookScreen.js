4edae0a32db8cf2f80f20bdd7fdc83185eeca7cc
Kevinatorn <kevin.kimaryo@gmail.com>
2018-01-21 15:55:44 +0100

    Anton fixes lol

--- src/components/screens/MyPageNavbarScreens/SongBookScreen.js
+++ src/components/screens/MyPageNavbarScreens/SongBookScreen.js
@@ -12,8 +12,9 @@ class SongBookScreen extends Component {
   constructor(props) {
     super(props);
     const data = [];
-    songs2014.dict.array.forEach(song => {
+    songs2014.dict.array.forEach((song, i) => {
       const item = {};
+      item.key = i;
       item.name = song.string[1];
       item.melody = song.string[2];
       item.text = song.string[3];
@@ -23,10 +24,6 @@ class SongBookScreen extends Component {
     this.state = {
       data
     };
-  }
-
-  componentWillMount() {
-    console.log(this.state.data);
   }
 
   getStrings() {
