e5f3704fe751cb2e0cec0c053f8b8fc403369a20
antongoransson <anton.goransson95@gmail.com>
2018-01-24 20:44:55 +0100

    Removed unused imports

--- src/components/screens/SplashScreen.js
+++ src/components/screens/SplashScreen.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { Animated, View, Image, Text, StatusBar, Easing } from 'react-native';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { NavigationActions } from 'react-navigation';
 import { getItem, getPopoverStatus, getFavoriteSections } from '../../helpers/LocalSave';
 import { dynamicSort } from '../../helpers/functions';
 import { BackgroundImage } from '../common';
-import { TOKEN_URL } from '../../helpers/Constants';
 import {
   setSections,
   setToken,
