6298eb60f99ef263afddb48a0b69e3ebbf13b6cd
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-03-07 14:12:22 +0100

    Add texts and textrendering

--- src/components/screens/TreasurehuntScreens/SwipeScreen.js
+++ src/components/screens/TreasurehuntScreens/SwipeScreen.js
@@ -1,39 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   View,
   Text,
-} from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import { connect } from 'react-redux';
-import GestureRecognizer from 'react-native-swipe-gestures';
+} from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { connect } from 'react-redux'
+import GestureRecognizer from 'react-native-swipe-gestures'
 import { Header, BackgroundImage, CountDown, CustomButton } from '../../common'
 import { HEIGHT, WIDTH, IS_IOS } from '../../../helpers/Constants'
 import { TREASURE_HUNT_SCREEN_STRINGS } from '../../../helpers/LanguageStrings'
 
 class SwipeScreen extends Component {
-  state= {
+  state = {
     counter: 0
   }
 
-  getStrings() {
-    const { language } = this.props;
-    const { fields } = TREASURE_HUNT_SCREEN_STRINGS;
-    const strings = {};
-    fields.forEach(field => (strings[field] = TREASURE_HUNT_SCREEN_STRINGS[field][language]));
-    return strings;
+  getStrings () {
+    const {language} = this.props
+    const {fields} = TREASURE_HUNT_SCREEN_STRINGS
+    const strings = {}
+    fields.forEach(field => (strings[field] = TREASURE_HUNT_SCREEN_STRINGS[field][language]))
+    return strings
   }
 
-  renderProgressButton(counter, value) {
+  renderProgressButton (counter, value) {
     return (
       <MaterialIcons
-        name={'radio-button-unchecked'}
-        size={15}
-        style={{ backgroundColor: 'transparent', color: counter === value ? 'blue' : 'grey' }}
+        name={'face'}
+        size={45}
+        style={{backgroundColor: 'transparent', color: counter === value ? 'white' : 'black'}}
       />
     )
   }
 
-  render() {
+  renderInfoText (counter, styles, strings) {
+    if (counter < 0 || counter > 2) {
+      return
+    }
+    let textInt = ''
+    switch (counter) {
+      case 0:
+        textInt = 'first'
+        break
+      case 1:
+        textInt = 'second'
+        break
+      case 2:
+        textInt = 'third'
+        break
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.infoHeaderStyle}>{strings[textInt + 'Header']}</Text>
+        <Text style={styles.infoTextStyle}>{strings[textInt + 'Body']}</Text>
+      </View>)
+  }
+
+  render () {
     const strings = this.getStrings()
     const {
       mainContainer,
@@ -41,49 +64,45 @@ class SwipeScreen extends Component {
       buttonContainer,
       countDownContainer,
       infoTextStyle,
+      infoHeaderStyle,
       progressButtonsStyle,
       startButtonStyle
     } = styles
     const config = {
       velocityThreshold: 0.3,
       directionalOffsetThreshold: 60
-    };
-    
-    const { counter } = this.state
+    }
+
+    const {counter} = this.state
     return (
       <GestureRecognizer
-        onSwipeUp={() => { if (counter < 2) this.setState({ counter: counter + 1 }) }}
-        onSwipeDown={() => { if (counter > 0) this.setState({ counter: counter - 1 }) }}
+        onSwipeUp={() => { if (counter < 2) this.setState({counter: counter + 1}) }}
+        onSwipeDown={() => { if (counter > 0) this.setState({counter: counter - 1}) }}
         config={config}
       >
-      <View style={mainContainer}>
-        <BackgroundImage pictureNumber={1} />
-        <Header title={strings.treasureHunt} />
-        <View style={countDownContainer}>
-          <Text style={textStyle}>{strings.timeLeft + ': '}</Text>
-          <CountDown endDate={this.props.screenProps.endDate} />
-        </View>
-        <View style={container}>
-          <View style={buttonContainer}>
-            <Text style={infoTextStyle}>{strings.info}</Text>
+        <View style={mainContainer}>
+          <BackgroundImage pictureNumber={1}/>
+          <Header title={strings.treasureHunt}/>
+          <View style={countDownContainer}>
+            <Text style={textStyle}>{strings.timeLeft + ': '}</Text>
+            <CountDown endDate={this.props.screenProps.endDate}/>
+          </View>
+          {this.renderInfoText(counter, styles, strings)}
+          {counter === 2 ? <View style={startButtonStyle}>
+            <CustomButton
+              style={'standardButton'}
+              onPress={() => this.props.navigation.navigate('GameScreen')}
+              text={strings.startButton}
+            />
+          </View> : null}
+          <View style={[progressButtonsStyle, {marginTop: counter === 2 ? 0 : 66}]}>
+            {this.renderProgressButton(counter, 0)}
+            {this.renderProgressButton(counter, 1)}
+            {this.renderProgressButton(counter, 2)}
           </View>
         </View>
-        {counter === 2 ?
-          <View style={startButtonStyle}>
-          <CustomButton
-          style={'standardButton'}
-          onPress={() => this.props.navigation.navigate('GameScreen')}
-          text={strings.startButton}
-          />
-          </View> : null }
-        <View style={[progressButtonsStyle, { marginTop: counter === 2 ? 0 : 66 }]}>
-          {this.renderProgressButton(counter, 0)}
-          {this.renderProgressButton(counter, 1)}
-          {this.renderProgressButton(counter, 2)}
-        </View>
-      </View>
       </GestureRecognizer>
-    );
+    )
   }
 }
 
@@ -97,8 +116,8 @@ const styles = {
     justifyContent: 'center',
   },
   countDownContainer: {
-     alignItems: 'center',
-     flexDirection: 'row',
+    alignItems: 'center',
+    flexDirection: 'row',
     justifyContent: 'center'
   },
   buttonContainer: {
@@ -110,12 +129,18 @@ const styles = {
     color: 'white',
     fontSize: 22
   },
-  infoTextStyle: {
+  infoHeaderStyle: {
     flex: 1,
     textAlign: 'center',
     flexWrap: 'wrap',
     color: 'white',
-    fontSize: 22
+    fontSize: 40,
+  },
+  infoTextStyle: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+    color: 'white',
+    fontSize: 22,
   },
   progressButtonsStyle: {
     flexDirection: 'row',
@@ -128,9 +153,9 @@ const styles = {
     alignItems: 'flex-end'
   }
 }
-const mapStateToProps = ({ currentLanguage }) => {
-  const { language } = currentLanguage;
-  return { language };
-};
+const mapStateToProps = ({currentLanguage}) => {
+  const {language} = currentLanguage
+  return {language}
+}
 
 export default connect(mapStateToProps, null)(SwipeScreen)
--- src/helpers/LanguageStrings.js
+++ src/helpers/LanguageStrings.js
@@ -1,131 +1,131 @@
 const loading = {
   SE: 'LADDAR ',
   EN: 'LOADING '
-};
+}
 const password = {
   SE: 'Lösenord',
   EN: 'Password'
-};
+}
 const shirtSize = {
   SE: 'Välj tröjstorlek',
   EN: 'Choose shirt size'
-};
+}
 const expiredTokenTitle = {
   SE: 'Sessionen har förfallit',
   EN: 'Session Expired'
-};
+}
 const expiredTokenMessage = {
   SE: 'Din session har tagit slut var vänlig logga in igen om du vill fortsätta',
   EN: 'Your session has expired please log in again to continue'
-};
+}
 const studentNation = {
   SE: 'Välj nation',
   EN: 'Choose your student nation'
-};
+}
 const driversLicense = {
   SE: 'Välj körkortstyp',
-  EN: "Choose type of driver's license"
-};
+  EN: 'Choose type of driver\'s license'
+}
 const errorMsg400 = {
   SE: 'Fel mail eller lösenord',
   EN: 'Wrong email or password'
-};
+}
 const errorMsg401 = {
   SE: 'Fel mail eller lösenord',
   EN: 'Wrong email or password'
-};
+}
 const errorMsg404 = {
   SE: 'Något gick fel...',
   EN: 'Something went wrong...'
-};
+}
 const errorMsg409EmailAndPersonalNumber = {
   SE: 'Mail och personnummer är redan i bruk',
   EN: 'Email and social security number is already in use'
-};
+}
 const errorMsg409Email = {
   SE: 'Mail är redan i bruk',
   EN: 'Email is already in use'
-};
+}
 const errorMsg409PersonalNumber = {
   SE: 'Personnummer är redan i bruk',
   EN: 'Social security number is already in use'
-};
+}
 const errorMsgInternal = {
   SE: 'Internt fel, var god försök igen senare',
   EN: 'Internal error, please try again later'
-};
+}
 const error = {
   SE: 'Fel',
   EN: 'Error'
-};
+}
 const firstName = {
   SE: 'Förnamn',
   EN: 'First name'
-};
+}
 const lastName = {
   SE: 'Efternamn',
   EN: 'Last name'
-};
+}
 const socialSecurityNumber = {
   SE: 'Personnummer',
   EN: 'Social security number'
-};
+}
 const email = {
   SE: 'Mail',
   EN: 'Email'
-};
+}
 const address = {
   SE: 'Adress',
   EN: 'Address'
-};
+}
 const postNumber = {
   SE: 'Postnummer',
   EN: 'Postal code'
-};
+}
 const city = {
   SE: 'Stad',
   EN: 'City'
-};
+}
 const phoneNumber = {
   SE: 'Telefonnummer',
   EN: 'Phone number'
-};
+}
 const foodPreference = {
   SE: 'Matpreferenser',
   EN: 'Food preferences'
-};
+}
 const cancel = {
   SE: 'Avbryt',
   EN: 'Cancel'
-};
+}
 const ok = {
   SE: 'OK',
   EN: 'OK'
-};
+}
 
 const profile = {
   SE: 'Min Profil',
   EN: 'My Profile'
-};
+}
 const registration = {
   SE: 'Min Registrering',
   EN: 'My Registration'
-};
+}
 
 const alertHeader = {
   SE: 'Bekräfta',
   EN: 'Confirm'
-};
+}
 
 const yes = {
   SE: 'JA',
   EN: 'YES'
-};
+}
 
 const no = {
   SE: 'NEJ',
   EN: 'NO'
-};
+}
 
 export const LOGIN_SCREEN_STRINGS = {
   fields: [
@@ -219,7 +219,7 @@ export const LOGIN_SCREEN_STRINGS = {
     SE: 'Tack, kolla din inkorg för ditt nya lösenord',
     EN: 'Thank you, check your inbox for your new password'
   }
-};
+}
 
 export const REGISTRATION_SCREEN_STRINGS = {
   fields: [
@@ -326,7 +326,7 @@ export const REGISTRATION_SCREEN_STRINGS = {
   driversLicense,
   driversLicenseArray: {
     SE: ['Jag har inget körkort', 'B', 'C', 'D', 'BE', 'CE', 'DE'],
-    EN: ["I do not have a driver's license", 'B', 'C', 'D', 'BE', 'CE', 'DE']
+    EN: ['I do not have a driver\'s license', 'B', 'C', 'D', 'BE', 'CE', 'DE']
   },
   shirtSize,
   shirtSizeArray: {
@@ -439,7 +439,7 @@ export const REGISTRATION_SCREEN_STRINGS = {
     SE:
       'En karnekompis är någon som du vill jobba med under eller i föreberedelserna till Lundakarnevalen. Skriv in din karnekompis mail i fältet! OBS, det finns ingen garanti för att du kommer tillsammans med din karnekompis.',
     EN:
-      "A BFF is someone you wish to work with during or in the preparation for Lundakarnevalen. Enter your BFF's email in the field. There are no guarantee that you will end up with your BFF."
+      'A BFF is someone you wish to work with during or in the preparation for Lundakarnevalen. Enter your BFF\'s email in the field. There are no guarantee that you will end up with your BFF.'
   },
   smallPleasuresHeader: {
     SE: 'Audition (Små nöjen)',
@@ -519,22 +519,22 @@ export const REGISTRATION_SCREEN_STRINGS = {
   },
   gdpr1: {
     SE:
-      'Jag är medveten om och godkänner att Lundakarnevalen samlar in mina personuppgifter för att:' +
-      ' kontrollera mitt Studentlundsmedlemsskap, registrera mig i Folkuniversitetets Studiecirklar' +
-      ' (för att kunna söka bidrag till Lundakarnevalens verksamheter kan studiecirklar förekomma, detta kommer att informeras om i respektive sektion)' +
-      ' samt användas tillsammans med Lundakarnevalens samarbetsorganisation ung media.',
+    'Jag är medveten om och godkänner att Lundakarnevalen samlar in mina personuppgifter för att:' +
+    ' kontrollera mitt Studentlundsmedlemsskap, registrera mig i Folkuniversitetets Studiecirklar' +
+    ' (för att kunna söka bidrag till Lundakarnevalens verksamheter kan studiecirklar förekomma, detta kommer att informeras om i respektive sektion)' +
+    ' samt användas tillsammans med Lundakarnevalens samarbetsorganisation ung media.',
     EN:
-      'I am aware of and allow the use of my personal information by Lundakarnevalen to: Control my membership in Studentlund, ' +
-      'register me for study circles at Folkuniversitetet (to be able to get benefits for the activities organized by Lundakarnevalen study circles might occur, the section will provide information about this)' +
-      ' and to be used together with ung media, a cooperation organization of Lundakarnevalen.'
+    'I am aware of and allow the use of my personal information by Lundakarnevalen to: Control my membership in Studentlund, ' +
+    'register me for study circles at Folkuniversitetet (to be able to get benefits for the activities organized by Lundakarnevalen study circles might occur, the section will provide information about this)' +
+    ' and to be used together with ung media, a cooperation organization of Lundakarnevalen.'
   },
   gdpr2: {
     SE:
-      'Jag är medveten om att Lundakarnevalen kommer att fota/filma och att jag kan förekomma i Lundakarnevalens bilder.' +
-      ' Jag godkänner att Lundakarnevalen använder sådant foto och film på Facebook, Instagram, Youtube och i karnevalsboxen.',
+    'Jag är medveten om att Lundakarnevalen kommer att fota/filma och att jag kan förekomma i Lundakarnevalens bilder.' +
+    ' Jag godkänner att Lundakarnevalen använder sådant foto och film på Facebook, Instagram, Youtube och i karnevalsboxen.',
     EN:
-      'I am aware that Lundakarnevalen will take pictures/movies and that I can appear in these. I allow Lundakarnevalen to' +
-      ' use these pictures and movies on Facebook, Instagram, Youtube and in karnevalsboxen.'
+    'I am aware that Lundakarnevalen will take pictures/movies and that I can appear in these. I allow Lundakarnevalen to' +
+    ' use these pictures and movies on Facebook, Instagram, Youtube and in karnevalsboxen.'
   },
   gdpr3: {
     SE: 'Jag är medveten om och godkänner att mina kontaktuppgifter behövs under hela år 2018.',
@@ -542,13 +542,13 @@ export const REGISTRATION_SCREEN_STRINGS = {
   },
   gdpr4: {
     SE:
-      'Om jag accepterar en post med mer ansvar godkänner jag att mina kontaktuppgifter sparas till nästa Lundakarneval,' +
-      ' för att göra en överlämning till en efterträdare.',
+    'Om jag accepterar en post med mer ansvar godkänner jag att mina kontaktuppgifter sparas till nästa Lundakarneval,' +
+    ' för att göra en överlämning till en efterträdare.',
     EN:
-      'If I accept a post with more responsibility I allow my contact information to be saved until the next Lundakarneval,' +
-      ' to make a handover to a successor.'
+    'If I accept a post with more responsibility I allow my contact information to be saved until the next Lundakarneval,' +
+    ' to make a handover to a successor.'
   }
-};
+}
 
 export const REGISTRATION_INFO_SCREEN_STRINGS = {
   fields: [
@@ -584,13 +584,13 @@ export const REGISTRATION_INFO_SCREEN_STRINGS = {
   },
   infoText1: {
     SE:
-      'Första steget är att skapa en profil, antingen här eller på Karnevalist.se.' +
-      ' När du skapat en profil tilldelas du ett profil-ID som' +
-      ' du använder vid incheckning under uppropsdagen.',
+    'Första steget är att skapa en profil, antingen här eller på Karnevalist.se.' +
+    ' När du skapat en profil tilldelas du ett profil-ID som' +
+    ' du använder vid incheckning under uppropsdagen.',
     EN:
-      'The first step is to create a profile, either here or at Karnevalist.se.' +
-      ' When you have created a profile, you are assigned a profile ID that you use' +
-      ' when checking in during the call day.'
+    'The first step is to create a profile, either here or at Karnevalist.se.' +
+    ' When you have created a profile, you are assigned a profile ID that you use' +
+    ' when checking in during the call day.'
   },
   infoText2: {
     SE: 'Välj vilka sektioner du vill vara delaktig i under karnevalen.',
@@ -604,7 +604,7 @@ export const REGISTRATION_INFO_SCREEN_STRINGS = {
     SE: 'Jag förstår',
     EN: 'I understand'
   }
-};
+}
 
 export const SECTION_SCREEN_STRINGS = {
   fields: ['title', 'refresh', 'popoverText', 'headerTitle'],
@@ -624,7 +624,7 @@ export const SECTION_SCREEN_STRINGS = {
     SE: 'Sektionsinfo',
     EN: 'Section Info'
   }
-};
+}
 
 export const NEWS_SCREEN_STRINGS = {
   fields: ['title'],
@@ -632,7 +632,7 @@ export const NEWS_SCREEN_STRINGS = {
     SE: 'Nyheter',
     EN: 'News'
   }
-};
+}
 
 export const HOME_SCREEN_STRINGS = {
   fields: [
@@ -711,7 +711,7 @@ export const HOME_SCREEN_STRINGS = {
     EN:
       'You will be able to check in when you are first in line. Until then you can choose sections but it will not be shown here on the home screen.'
   }
-};
+}
 
 export const SETTINGS_SCREEN_STRINGS = {
   fields: [
@@ -744,7 +744,7 @@ export const SETTINGS_SCREEN_STRINGS = {
     SE: 'Du loggas nu ut',
     EN: 'You will now be logged out'
   }
-};
+}
 
 export const SECTION_ITEM_SCREEN_STRINGS = {
   fields: ['messageStart', 'messageEndAdd', 'messageEndRemove'],
@@ -760,7 +760,7 @@ export const SECTION_ITEM_SCREEN_STRINGS = {
     SE: '" borttagen',
     EN: '" removed'
   }
-};
+}
 
 export const SONGBOOK_SCREEN_STRINGS = {
   fields: ['title', 'headerTitle'],
@@ -772,7 +772,7 @@ export const SONGBOOK_SCREEN_STRINGS = {
     SE: 'Sångtext',
     EN: 'Song Text'
   }
-};
+}
 
 export const CONFIRM_PAGE_STRINGS = {
   fields: [
@@ -857,7 +857,7 @@ export const CONFIRM_PAGE_STRINGS = {
     SE: 'Registrering stängd',
     EN: 'Registration closed'
   }
-};
+}
 
 export const MY_PROFILE_SCREEN_STRINGS = {
   fields: [
@@ -985,7 +985,7 @@ export const MY_PROFILE_SCREEN_STRINGS = {
   ok,
   yes,
   no
-};
+}
 
 export const MY_REGISTRATION_SCREEN_STRINGS = {
   fields: ['title', 'emptyListMessage'],
@@ -994,24 +994,24 @@ export const MY_REGISTRATION_SCREEN_STRINGS = {
     SE: 'Du har inte valt några sektioner',
     EN: 'You have not chosen any sections'
   }
-};
+}
 
 export const LOADING_STRINGS = {
   fields: ['loading'],
   loading
-};
+}
 
 export const COUNT_DOWN_STRINGS = {
   fields: ['karneval', 'upprop'],
   karneval: {
     SE: 'Det är karneval!',
-    EN: "It's carnival!"
+    EN: 'It\'s carnival!'
   },
   upprop: {
     SE: 'Det är upprop!',
-    EN: "It's call day!"
+    EN: 'It\'s call day!'
   }
-};
+}
 
 export const ERROR_MSG_INPUT_FIELD = {
   fields: [
@@ -1086,14 +1086,20 @@ export const ERROR_MSG_INPUT_FIELD = {
     SE: 'Vänligen se till att inga obligatoriska fält är tomma',
     EN: 'Please make sure no mandatory fields are left empty'
   }
-};
+}
 
 export const TREASURE_HUNT_SCREEN_STRINGS = {
   fields: [
     'treasureHunt',
     'startButton',
     'timeLeft',
-    'info'
+    'info',
+    'firstHeader',
+    'firstBody',
+    'secondHeader',
+    'secondBody',
+    'thirdHeader',
+    'thirdBody',
   ],
   treasureHunt: {
     SE: 'Skattjakt',
@@ -1108,7 +1114,31 @@ export const TREASURE_HUNT_SCREEN_STRINGS = {
     EN: 'TIME LEFT'
   },
   info: {
-    SE: 'INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO',
+    SE: 'Välkommen till vår skattjakt! INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO',
     EN: 'INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO'
+  },
+  firstHeader: {
+    SE: 'Välkommen till vår skattjakt!',
+    EN: 'Welcome to our treasure hunt! '
+  },
+  firstBody: {
+    SE: 'Lundakarnevalen anordnar en skattjakt där deltagarna letar sig igenom lund för att hitta den hemliga plats där Sture gömt sig. De första 25 kommer få ett pris!',
+    EN: 'Lundakarnevalen arranges a treasure hunt where the contestans compete to find Sture, who is hiding at a the (top) secret location. The first 25 to find him will recieve a special prize!'
+  },
+  secondHeader: {
+    SE: 'Hur kommer det gå till?',
+    EN: 'How will it work?'
+  },
+  secondBody: {
+    SE: 'Se till att du har igång GPS, vrid sen din mobil i olika riktningar and lägg märke till när den vibrerar. Detta betyder att den hemliga platsen är åt det hållet!',
+    EN: 'Ensure your location sensor is on, then turn your phone various directions and notice when it vibrates. That means the secret location is that way!',
+  },
+  thirdHeader: {
+    SE: 'Och sen då?',
+    EN: 'And then what?'
+  },
+  thirdBody: {
+    SE: 'När du kommit nära Sture får du en QR-kod att visa honom! \nJag förstår, nu kör vi!',
+    EN: 'When you get close to Sture, you will get a QR code to show him! \nI got it, let\'s GO!'
   }
 }
