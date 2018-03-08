bc56750d7e878d3ffecdb5f2cf5805e96be59493
antongoransson <anton.goransson95@gmail.com>
2018-03-08 11:03:45 +0100

    Refactor Listitem done

--- src/components/common/ListItem/index.js
+++ src/components/common/ListItem/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { TouchableOpacity, Text, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import { WIDTH } from 'helpers/Constants';
+import { ITEM_WIDTH, styles } from './styles';
 
-const ITEM_WIDTH = WIDTH - 16;
 const months = [
   'Jan',
   'Feb',
@@ -19,123 +19,77 @@ const months = [
   'Dec'
 ];
 
-class ListItem extends Component {
-  renderDateView(itemDate) {
-    const { dateViewStyle, dateStyle } = styles;
-    const dateParts = itemDate.split('T')[0].split('-');
-    if (itemDate) {
-      return (
-        <View style={dateViewStyle}>
-          <Text style={dateStyle}>{dateParts[2]}</Text>
-          <Text style={dateStyle}>{months[dateParts[1] - 1]}</Text>
-        </View>
-      );
-    }
-  }
-
-  render() {
-    const {
-      containerStyle,
-      titleStyle,
-      contentStyle,
-      continueIconIndicatorStyle
-    } = styles;
-    const {
-      title = '',
-      icon = '',
-      infoText = '',
-      itemDate = '',
-      onPress
-    } = this.props;
+const renderDateView = itemDate => {
+  const { dateViewStyle, dateStyle } = styles;
+  const dateParts = itemDate.split('T')[0].split('-');
+  if (itemDate) {
     return (
-      <TouchableOpacity onPress={() => onPress()} style={containerStyle}>
-        <View style={{ flexDirection: 'row' }}>
-          {this.renderDateView(itemDate)}
-          <View style={{ flexDirection: 'column', justifyContent: 'center' }}>
-            <Text
-              numberOfLines={1}
-              style={[
-                titleStyle,
-                {
-                  width: itemDate === '' ? ITEM_WIDTH * 0.85 : ITEM_WIDTH * 0.7
-                }
-              ]}
-            >
-              {icon === '' ? null : (
-                <MaterialIcons name={icon} size={15} color="#F7A021" />
-              )}
-              {icon === '' || icon === null ? title : ` ${  title}`}
-            </Text>
-            {infoText === '' ? null : (
-              <Text
-                numberOfLines={1}
-                style={[
-                  contentStyle,
-                  {
-                    width:
-                      itemDate === '' ? ITEM_WIDTH * 0.85 : ITEM_WIDTH * 0.7
-                  }
-                ]}
-              >
-                {infoText}
-              </Text>
-            )}
-          </View>
-        </View>
-        <View>
-          <MaterialIcons
-            name="keyboard-arrow-right"
-            style={continueIconIndicatorStyle}
-            color="#F7A021"
-            size={this.props.icon === 'done' ? 40 : 50}
-          />
-        </View>
-      </TouchableOpacity>
+      <View style={dateViewStyle}>
+        <Text style={dateStyle}>{dateParts[2]}</Text>
+        <Text style={dateStyle}>{months[dateParts[1] - 1]}</Text>
+      </View>
     );
   }
-}
+  return null;
+};
 
-const styles = {
-  containerStyle: {
-    height: 60,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: ITEM_WIDTH,
-    borderWidth: 1,
-    marginTop: 8,
-    borderColor: '#F7A021'
-  },
-  titleStyle: {
-    fontSize: 20,
-    marginLeft: 8,
-    backgroundColor: 'transparent',
-    fontFamily: 'Avenir Next Medium',
-    color: '#F7A021'
-  },
-  contentStyle: {
-    fontSize: 14,
-    marginLeft: 8,
-    backgroundColor: 'transparent',
-    fontFamily: 'Avenir Next Medium',
-    color: '#333'
-  },
-  dateViewStyle: {
-    height: 60,
-    width: ITEM_WIDTH * 0.15,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F7A021'
-  },
-  dateStyle: {
-    fontSize: 16,
-    color: 'white',
-    backgroundColor: 'transparent',
-    fontFamily: 'Avenir Next Bold'
-  },
-  continueIconIndicatorStyle: {
-    backgroundColor: 'transparent'
+const getTitleStyle = itemDate => [
+  styles.titleStyle,
+  {
+    width: itemDate === '' ? ITEM_WIDTH * 0.85 : ITEM_WIDTH * 0.7
   }
+];
+const getContentStyle = itemDate => [
+  styles.contentStyle,
+  {
+    width: itemDate === '' ? ITEM_WIDTH * 0.85 : ITEM_WIDTH * 0.7
+  }
+];
+
+const ListItem = ({ title, icon, infoText, itemDate, onPress }) => {
+  const { containerStyle, continueIconStyle, innerContainerStyle } = styles;
+  return (
+    <TouchableOpacity onPress={() => onPress()} style={containerStyle}>
+      <View style={{ flexDirection: 'row' }}>
+        {renderDateView(itemDate)}
+        <View style={innerContainerStyle}>
+          <Text numberOfLines={1} style={getTitleStyle(itemDate)}>
+            {icon === '' ? null : (
+              <MaterialIcons name={icon} size={15} color="#F7A021" />
+            )}
+            {icon === '' || icon === null ? title : ` ${title}`}
+          </Text>
+          {infoText === '' ? null : (
+            <Text numberOfLines={1} style={getContentStyle(itemDate)}>
+              {infoText}
+            </Text>
+          )}
+        </View>
+      </View>
+      <View>
+        <MaterialIcons
+          name="keyboard-arrow-right"
+          style={continueIconStyle}
+          color="#F7A021"
+          size={icon === 'done' ? 40 : 50}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+ListItem.defaultProps = {
+  title: '',
+  icon: '',
+  itemDate: '',
+  infoText: ''
+};
+
+ListItem.propTypes = {
+  title: PropTypes.string,
+  icon: PropTypes.string,
+  itemDate: PropTypes.string,
+  infoText: PropTypes.string,
+  onPress: PropTypes.func.isRequired
 };
 
 export { ListItem };
---
+++ src/components/common/ListItem/styles.js
@@ -0,0 +1,50 @@
+import { WIDTH } from 'helpers/Constants';
+
+export const ITEM_WIDTH = WIDTH - 16;
+
+export const styles = {
+  containerStyle: {
+    height: 60,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: ITEM_WIDTH,
+    borderWidth: 1,
+    marginTop: 8,
+    borderColor: '#F7A021'
+  },
+  titleStyle: {
+    fontSize: 20,
+    marginLeft: 8,
+    backgroundColor: 'transparent',
+    fontFamily: 'Avenir Next Medium',
+    color: '#F7A021'
+  },
+  contentStyle: {
+    fontSize: 14,
+    marginLeft: 8,
+    backgroundColor: 'transparent',
+    fontFamily: 'Avenir Next Medium',
+    color: '#333'
+  },
+  dateViewStyle: {
+    height: 60,
+    width: ITEM_WIDTH * 0.15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F7A021'
+  },
+  dateStyle: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontFamily: 'Avenir Next Bold'
+  },
+  continueIconStyle: {
+    backgroundColor: 'transparent'
+  },
+  innerContainerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }
+};
