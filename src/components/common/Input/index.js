dddfd4ead491c10ab9a983c46c05c05983c1050e
antongoransson <anton.goransson95@gmail.com>
2018-03-08 10:00:17 +0100

    Refactor input done

--- src/components/common/Input/index.js
+++ src/components/common/Input/index.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
-import { View, TextInput, Animated, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  TextInput,
+  Animated,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+import PropTypes from 'prop-types';
 import { FontAwesome } from '@expo/vector-icons';
+import { styles } from './styles';
+
+const getContainerStyle = (props, borderColor) => {
+  const { hasError, extraContainerStyle, value, width } = props;
+  if (value !== '' && hasError) borderColor = 'red';
+  return [styles.containerStyle, extraContainerStyle, { width, borderColor }];
+};
+
+const getInputStyle = ({ multiline, extraInputStyle }) => [
+  styles.inputStyle,
+  { height: multiline ? 133 : 44, paddingTop: multiline ? 15 : 10 },
+  extraInputStyle
+];
 
 class Input extends Component {
   constructor(props) {
@@ -16,32 +36,6 @@ class Input extends Component {
     if (this.props.value !== '') this.inputSelected();
   }
 
-  inputSelected() {
-    Animated.parallel([
-      Animated.timing(this.state.fontSize, { toValue: 10, duration: 150 }),
-      Animated.timing(this.state.position, { toValue: { x: 9, y: 0 }, duration: 150 })
-    ]).start();
-    this.setState({ borderColor: '#F7A021' });
-  }
-
-  inputDeselected() {
-    const { value } = this.props;
-    if (value === '') {
-      Animated.parallel([
-        Animated.timing(this.state.fontSize, { toValue: 18, duration: 150 }),
-        Animated.timing(this.state.position, { toValue: { x: 9, y: 11 }, duration: 150 })
-      ]).start();
-      this.setState({ borderColor: 'black' });
-    }
-  }
-
-  addWarningText() {
-    const { warningMessage, hasError = false, value } = this.props;
-    if (hasError && value !== '') {
-      return <Text style={styles.warningTextStyle}>{warningMessage}</Text>;
-    }
-  }
-
   getPlaceholderStyle() {
     const { fontSize, position } = this.state;
     return {
@@ -51,18 +45,9 @@ class Input extends Component {
       position: 'absolute',
       top: position.y,
       left: position.x,
-      color: '#F7A021'
+      color: this.getTextColor(),
+      ...this.props.extraPlaceHolderStyle
     };
-  }
-
-  focus() {
-    this.refs.input.focus();
-  }
-
-  getBorderColor() {
-    const { hasError, value } = this.props;
-    if (value !== '' && hasError) return 'red';
-    return this.state.borderColor;
   }
 
   getTextColor() {
@@ -71,66 +56,96 @@ class Input extends Component {
     return '#F7A021';
   }
 
+  focus() {
+    this.input.focus();
+  }
+
+  addWarningText() {
+    const { warningMessage, hasError, value } = this.props;
+    if (hasError && value !== '') {
+      return <Text style={styles.warningTextStyle}>{warningMessage}</Text>;
+    }
+    return null;
+  }
+
+  inputDeselected() {
+    const { value } = this.props;
+    if (value === '') {
+      Animated.parallel([
+        Animated.timing(this.state.fontSize, { toValue: 18, duration: 150 }),
+        Animated.timing(this.state.position, {
+          toValue: { x: 9, y: 11 },
+          duration: 150
+        })
+      ]).start();
+      this.setState({ borderColor: 'black' });
+    }
+  }
+
+  inputSelected() {
+    Animated.parallel([
+      Animated.timing(this.state.fontSize, { toValue: 10, duration: 150 }),
+      Animated.timing(this.state.position, {
+        toValue: { x: 9, y: 0 },
+        duration: 150
+      })
+    ]).start();
+    this.setState({ borderColor: '#F7A021' });
+  }
+
+  handleOnFocus() {
+    if (typeof this.props.scrollToInput !== 'undefined') {
+      this.props.scrollToInput(this.state.screenPosition);
+    }
+    this.inputSelected();
+  }
+
   render() {
-    const { inputStyle, containerStyle, iconTouchableStyle, innerContainerStyle } = styles;
+    const { iconTouchableStyle, innerContainerStyle } = styles;
     const {
       value,
-      width,
       placeholder,
       secureText,
-      autoCorrect = false,
-      autoCapitalize = 'sentences',
-      editable = true,
-      keyboardType = 'default',
-      extraContainerStyle,
-      extraInputStyle,
-      multiline = false,
-      numberOfLines = 1,
-      extraPlaceHolderStyle,
+      autoCorrect,
+      autoCapitalize,
+      editable,
+      keyboardType,
+      multiline,
+      numberOfLines,
       returnKeyType,
-      onSubmitEditing = () => {},
-      autoFocus = false,
+      onChangeText,
+      onSubmitEditing,
+      autoFocus,
       icon,
-      iconOnPress = () => {},
-      maxLength = 50
+      iconOnPress,
+      maxLength
     } = this.props;
     return (
       <View
         onLayout={event =>
-          this.setState({ screenPosition: (multiline ? 200 : 100) + event.nativeEvent.layout.y })
+          this.setState({
+            screenPosition: (multiline ? 200 : 100) + event.nativeEvent.layout.y
+          })
         }
-        style={[containerStyle, extraContainerStyle, { width, borderColor: this.getBorderColor() }]}
+        style={getContainerStyle(this.props, this.state.borderColor)}
       >
         {placeholder === '' ? null : (
-          <Animated.Text
-            style={[
-              this.getPlaceholderStyle(),
-              { color: this.getTextColor() },
-              extraPlaceHolderStyle
-            ]}
-          >
+          <Animated.Text style={this.getPlaceholderStyle()}>
             {placeholder}
           </Animated.Text>
         )}
         {this.addWarningText()}
         <View style={innerContainerStyle}>
           <TextInput
-            ref={'input'}
-            onFocus={() => {
-              if (typeof this.props.scrollToInput !== 'undefined') {
-                this.props.scrollToInput(this.state.screenPosition);
-              }
-              this.inputSelected();
+            ref={ref => {
+              this.input = ref;
             }}
-            underlineColorAndroid={'transparent'}
+            onFocus={() => this.handleOnFocus()}
+            underlineColorAndroid="transparent"
             onEndEditing={() => this.inputDeselected()}
-            onChangeText={text => this.props.onChangeText(text)}
+            onChangeText={text => onChangeText(text)}
             value={value}
-            style={[
-              inputStyle,
-              { height: multiline ? 133 : 44, paddingTop: multiline ? 15 : 10 },
-              extraInputStyle
-            ]}
+            style={getInputStyle(this.props)}
             autoCapitalize={autoCapitalize}
             secureTextEntry={secureText}
             autoCorrect={autoCorrect}
@@ -155,40 +170,54 @@ class Input extends Component {
   }
 }
 
-const styles = {
-  containerStyle: {
-    marginBottom: 8,
-    backgroundColor: 'white',
-    borderRadius: 2,
-    borderWidth: 1
-  },
-  iconTouchableStyle: {
-    width: 44,
-    height: 44,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'transparent'
-  },
-  inputStyle: {
-    paddingLeft: 8,
-    paddingRight: 8,
-    color: '#000',
-    fontFamily: 'Avenir Next Medium',
-    flex: 1,
-    fontSize: 16,
-    textAlignVertical: 'top'
-  },
-  warningTextStyle: {
-    color: 'red',
-    fontSize: 10,
-    position: 'absolute',
-    right: 5,
-    fontFamily: 'Avenir Next Medium'
-  },
-  innerContainerStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
+Input.defaultProps = {
+  autoCapitalize: 'sentences',
+  autoCorrect: false,
+  autoFocus: false,
+  editable: true,
+  extraContainerStyle: null,
+  extraInputStyle: null,
+  extraPlaceHolderStyle: null,
+  icon: null,
+  hasError: false,
+  keyboardType: 'default',
+  maxLength: 50,
+  multiline: false,
+  numberOfLines: 1,
+  onChangeText: () => {},
+  onSubmitEditing: () => {},
+  iconOnPress: () => {},
+  returnKeyType: null,
+  scrollToInput: undefined,
+  secureText: false,
+  warningMessage: '',
+  width: null
+};
+
+Input.propTypes = {
+  autoCapitalize: PropTypes.string,
+  autoCorrect: PropTypes.bool,
+  autoFocus: PropTypes.bool,
+  extraContainerStyle: PropTypes.shape(),
+  extraInputStyle: PropTypes.shape(),
+  extraPlaceHolderStyle: PropTypes.shape(),
+  editable: PropTypes.bool,
+  hasError: PropTypes.bool,
+  icon: PropTypes.string,
+  iconOnPress: PropTypes.func,
+  keyboardType: PropTypes.string,
+  maxLength: PropTypes.number,
+  multiline: PropTypes.bool,
+  numberOfLines: PropTypes.number,
+  onChangeText: PropTypes.func,
+  onSubmitEditing: PropTypes.func,
+  returnKeyType: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+  scrollToInput: PropTypes.func,
+  secureText: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+  warningMessage: PropTypes.string,
+  width: PropTypes.number
 };
 
 export { Input };
---
+++ src/components/common/Input/styles.js
@@ -0,0 +1,35 @@
+export const styles = {
+  containerStyle: {
+    marginBottom: 8,
+    backgroundColor: 'white',
+    borderRadius: 2,
+    borderWidth: 1
+  },
+  iconTouchableStyle: {
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  inputStyle: {
+    paddingLeft: 8,
+    paddingRight: 8,
+    color: '#000',
+    fontFamily: 'Avenir Next Medium',
+    flex: 1,
+    fontSize: 16,
+    textAlignVertical: 'top'
+  },
+  warningTextStyle: {
+    color: 'red',
+    fontSize: 10,
+    position: 'absolute',
+    right: 5,
+    fontFamily: 'Avenir Next Medium'
+  },
+  innerContainerStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+};
