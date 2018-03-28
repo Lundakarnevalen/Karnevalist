18c08899237f3948461431fcf2e2b4cc98a77b50
easklund <dat14eas@student.lu.se>
2018-03-28 17:40:15 +0200

    removed an unused function in function.js

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -34,29 +34,3 @@ export function getStrings(language, COMPONENT_STRINGS) {
   });
   return strings;
 }
-
-// takes the form field value and returns true on valid number
-function valid_credit_card(value) {
-  //   // accept only digits, dashes or spaces
-  //   if (/[^0-9-\s]+/.test(value)) return false;
-  //
-  //   // The Luhn Algorithm. It's so pretty.
-  //   let nCheck = 0,
-  //     nDigit = 0,
-  //     bEven = false;
-  //   value = value.replace(/\D/g, '');
-  //
-  //   for (let n = value.length - 1; n >= 0; n--) {
-  //     var cDigit = value.charAt(n),
-  //       nDigit = parseInt(cDigit, 10);
-  //
-  //     if (bEven) {
-  //       if ((nDigit *= 2) > 9) nDigit -= 9;
-  //     }
-  //
-  //     nCheck += nDigit;
-  //     bEven = !bEven;
-  //   }
-  //
-  //   return nCheck % 10 == 0;
-}
