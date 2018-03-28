d5d278264f582845d8822df12c70d51843a4aedc
antongoransson <anton.goransson95@gmail.com>
2018-03-28 23:41:37 +0200

    Add search and sectionheaders to songbook

--- src/components/screens/MyPageNavbarScreens/SongBookScreen/index.js
+++ src/components/screens/MyPageNavbarScreens/SongBookScreen/index.js
@@ -1,30 +1,61 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, SectionList, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Header, BackgroundImage, ListItem } from '~/src/components/common';
+import {
+  Header,
+  BackgroundImage,
+  ListItem,
+  Input
+} from '~/src/components/common';
 import { SONGBOOK_SCREEN_STRINGS } from '~/src/helpers/LanguageStrings';
 import { dynamicSort, getStrings } from '~/src/helpers/functions';
-import { HEIGHT, IS_IOS } from '~/src/helpers/Constants';
-import songs2014 from '~/assets/songbook/songs2014.json';
+import { HEIGHT, IS_IOS, WIDTH } from '~/src/helpers/Constants';
+import songs2018 from '~/assets/songbook/songs2018.json';
+import { styles } from './styles';
 
+const NBR_SONGS = 32;
 class SongBookScreen extends Component {
   constructor(props) {
     super(props);
-    const data = songs2014.dict.array.map((song, i) => ({
+    const data = songs2018.dict.array.map((song, i) => ({
       key: i,
       name: song.string[1],
       melody: song.string[2],
-      text: song.string[3]
+      text: song.string[3],
+      category: song.string[0]
     }));
-    data.sort(dynamicSort('name'));
+    const sections = songs2018.dict.array
+      .map(song => song.string[0])
+      .filter((item, i, ar) => ar.indexOf(item) === i);
+    const allData = sections.map(s => ({
+      title: s,
+      data: data.filter(d => d.category === s).sort(dynamicSort('name'))
+    }));
     this.state = {
-      data
+      allData,
+      currentData: allData,
+      input: ''
     };
   }
 
   getLanguageStrings() {
     return getStrings(this.props.language, SONGBOOK_SCREEN_STRINGS);
+  }
+
+  filterData(input) {
+    input = input.toLowerCase();
+    const { allData } = this.state;
+    const currentData = allData.map(({ title, data }) => ({
+      title,
+      data: data.filter(
+        d =>
+          d.name.toLowerCase().includes(input) ||
+          d.melody.toLowerCase().includes(input) ||
+          d.category.toLowerCase().includes(input)
+      )
+    }));
+    this.setState({ input, currentData });
   }
 
   render() {
@@ -36,13 +67,25 @@ class SongBookScreen extends Component {
         <View>
           <Header title={strings.title} />
         </View>
-        <FlatList
+        <Input
+          width={WIDTH * 2 / 3}
+          placeholder="Search.."
+          value={this.state.input}
+          onChangeText={input => this.filterData(input)}
+        />
+        <SectionList
           style={{ height: HEIGHT - (IS_IOS ? 113 : 135) }}
-          data={this.state.data}
-          contentContainerStyle={{ alignItems: 'center', paddingBottom: 60 }}
+          sections={this.state.currentData}
+          contentContainerStyle={{ alignItems: 'center', paddingBottom: 100 }}
+          renderSectionHeader={({ section }) =>
+            section.data && section.data.length > 0 ? (
+              <Text style={styles.sectionHeaderStyle}> {section.title} </Text>
+            ) : null
+          }
           renderItem={({ item }) => (
             <ListItem
               title={item.name}
+              infoText={item.melody}
               onPress={() =>
                 navigation.navigate('SongScreen', {
                   headerTitle: strings.headerTitle,
@@ -53,6 +96,8 @@ class SongBookScreen extends Component {
               }
             />
           )}
+          initialNumToRender={NBR_SONGS}
+          keyExtractor={(item, index) => index}
         />
       </View>
     );
