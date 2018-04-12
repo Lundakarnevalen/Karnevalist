6bbd80a25302c693e223fdedea755f89af027671
antongoransson <anton.goransson95@gmail.com>
2018-04-12 15:01:28 +0200

    Add karnevalid assets

---
+++ assets/images/KarnevalID/index.js
@@ -0,0 +1,13 @@
+const base = require('./Base.png');
+const baseBig = require('./BaseBig.png');
+const cup = require('./CupBig.png');
+const cupRowRight = require('./CupRowRight.png');
+const cupRowLeft = require('./CupRowLeft.png');
+
+export const karnevalID = {
+  cup,
+  cupRowRight,
+  cupRowLeft,
+  base,
+  baseBig
+};
