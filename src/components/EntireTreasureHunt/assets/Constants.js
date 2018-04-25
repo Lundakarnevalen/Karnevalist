bb1b6725944a66ba991946171ff6261b203228d3
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-04-25 16:03:34 +0200

    new treasure location

--- src/components/EntireTreasureHunt/assets/Constants.js
+++ src/components/EntireTreasureHunt/assets/Constants.js
@@ -6,9 +6,9 @@ export const WIDTH = Dimensions.get('window').width
 export const IS_IOS = Platform.OS === 'ios'
 
 // for the treasurehunt
-export const treasureLongitude = 13.2037983
-export const treasureLatitidue = 55.7106452
+export const treasureLongitude = 13.211214
+export const treasureLatitidue = 55.717583
 export const startDate = new Date(2018, 3, 29, 15, 0, 0)
 export const endDate = new Date(2018, 3, 29, 16, 30, 0)
-// export const basePath = 'https://api.karnevalist.se/' THESE NEED TO BE RESET ON UPATE
+// export const basePath = 'https://api.karnevalist.se/' THESE NEED TO BE RESET ON UPDATE
 export const basePath = 'https://api.10av10.com'
